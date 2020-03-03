61bfac159d0f26ca70a7cc57a4e36f08e532ad33
jsoyee <mail.zhang.yee@gmail.com>
2020-03-02 17:46:38 -0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -23,7 +23,8 @@ function get_pokemon_layer_from_map_items(map_items) {
       var map_item = map_items[i];
       var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitute"]), 
                                                {icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png'});
-map.entities.push(pushpin);
+       map.entities.push(pushpin);
+  }
 //   var pushpins = []
 //   for (var i in map_items) {
 //       var map_item = map_items[i];
