ea376dd2173da80700f5ea8984ec39bb5a8bdd78
jsoyee <mail.zhang.yee@gmail.com>
2020-03-02 18:51:45 -0800

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -36,10 +36,10 @@ function get_counter_down_time_from_expire_epoch(epoch) {
 }
     
 // 2. Create pokenmon image on map
-function get_pokemon_layer_from_map_items(map_items) {
+function get_pokemon_layer_from_map_items(map_manager.map_items) {
   var layer = new Microsoft.Maps.Layer();
-  for (var i in map_items) {
-      var map_item = map_items[i];
+  for (var i in map_manager.map_items) {
+      var map_item = map_manager.map_items[i];
       var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitute"]), 
                                                {icon: 'images/pushpin_images/pokemon/' + map_item['pokenmon_id'] + '.png', 
                                                title: get_counter_down_time_from_expire_epoch(map_item['expire'])});
@@ -52,7 +52,7 @@ function get_pokemon_layer_from_map_items(map_items) {
 
 
 function add_pokemon_layer() {
-    var pokenmon_layer = get_pokemon_layer_from_map_items(map_items)
+    var pokenmon_layer = get_pokemon_layer_from_map_items(map_manager.map_items)
     map.layers.insert(pokenmon_layer);
 }
 
@@ -60,7 +60,7 @@ function add_pokemon_layer() {
 // 3. Add pokenmon count down refresh
 function refresh_pokemon_layer() {
 // Prepare new layer
-    var pokenmon_layer = get_pokemon_layer_from_map_items(map_items)
+    var pokenmon_layer = get_pokemon_layer_from_map_items(map_manager.map_items)
 
 // Remove old layer
     map.layers.clear()
